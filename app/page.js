e5cd75d41da7d4bf129d5c79c30ee35ece1bbe66
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-03-25 15:03:58 +0430

    14-real-template lesson

--- app/page.js
+++ app/page.js
@@ -1,95 +1,145 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	return (
+		<div>
+			<section id="home" className="welcome-hero">
+				<div className="top-area">
+					<div className="header-area">
+						<nav className="navbar navbar-default bootsnav  navbar-sticky navbar-scrollspy"
+							 data-minus-value-desktop="70" data-minus-value-mobile="55" data-speed="1000">
+							<div className="container">
+								<div className="navbar-header">
+									<button type="button" className="navbar-toggle" data-toggle="collapse"
+											data-target="#navbar-menu">
+										<i className="fa fa-bars"></i>
+									</button>
+									<a className="navbar-brand" href="#">carvilla<span></span></a>
+								</div>
+								<div className="collapse navbar-collapse menu-ui-design" id="navbar-menu">
+									<ul className="nav navbar-nav navbar-right" data-in="fadeInDown"
+										data-out="fadeOutUp">
+										<li className=" scroll active"><a href="#">home</a></li>
+										<li className="scroll"><a href="#">service</a></li>
+										<li className="scroll"><a href="#">featured cars</a></li>
+										<li className="scroll"><a href="#">new cars</a></li>
+										<li className="scroll"><a href="#">brands</a></li>
+										<li className="scroll"><a href="#">contact</a></li>
+									</ul>
+								</div>
+							</div>
+						</nav>
+					</div>
+					<div className="clearfix"></div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+				</div>
+
+				<div className="container">
+					<div className="welcome-hero-txt">
+						<h2>get your desired car in resonable price</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+							ut labore et dolore magna aliqua.
+						</p>
+						<button className="welcome-btn" onClick="window.location.href='#'">contact us</button>
+					</div>
+				</div>
+
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							<div className="model-search-content">
+								<div className="row">
+									<div className="col-md-offset-1 col-md-2 col-sm-12">
+										<div className="single-model-search">
+											<h2>select year</h2>
+											<div className="model-select-icon">
+												<select className="form-control">
+													<option value="default">year</option>
+													<option value="2018">2018</option>
+													<option value="2017">2017</option>
+													<option value="2016">2016</option>
+												</select>
+											</div>
+										</div>
+										<div className="single-model-search">
+											<h2>body style</h2>
+											<div className="model-select-icon">
+												<select className="form-control">
+
+													<option value="default">style</option>
+													<option value="sedan">sedan</option>
+													<option value="van">van</option>
+													<option value="roadster">roadster</option>
+												</select>
+											</div>
+										</div>
+									</div>
+									<div className="col-md-offset-1 col-md-2 col-sm-12">
+										<div className="single-model-search">
+											<h2>select make</h2>
+											<div className="model-select-icon">
+												<select className="form-control">
+													<option value="default">make</option>
+													<option value="toyota">toyota</option>
+													<option value="holden">holden</option>
+													<option value="maecedes-benz">maecedes-benz.</option>
+												</select>
+											</div>
+										</div>
+										<div className="single-model-search">
+											<h2>car condition</h2>
+											<div className="model-select-icon">
+												<select className="form-control">
+
+													<option value="default">condition</option>
+													<option value="something">something</option>
+													<option value="something">something</option>
+													<option value="something">something</option>
+												</select>
+											</div>
+										</div>
+									</div>
+									<div className="col-md-offset-1 col-md-2 col-sm-12">
+										<div className="single-model-search">
+											<h2>select model</h2>
+											<div className="model-select-icon">
+												<select className="form-control">
+													<option value="default">model</option>
+													<option value="kia-rio">kia-rio</option>
+													<option value="mitsubishi">mitsubishi</option>
+													<option value="ford">ford</option>
+
+												</select>
+											</div>
+										</div>
+										<div className="single-model-search">
+											<h2>select price</h2>
+											<div className="model-select-icon">
+												<select className="form-control">
+
+													<option value="default">price</option>
+													<option value="$0.00">$0.00</option>
+													<option value="$0.00">$0.00</option>
+													<option value="$0.00">$0.00</option>
+
+												</select>
+											</div>
+										</div>
+									</div>
+									<div className="col-md-2 col-sm-12">
+										<div className="single-model-search text-center">
+											<button className="welcome-btn model-search-btn"
+													onClick="window.location.href='#'">
+												search
+											</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</section>
+		</div>
+	);
 }
