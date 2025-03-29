b90b4820297347a207730d02780aed7d451e33d5
MortazaAkhlaqi <akhlaqimortaza1@gmail.com>
2025-03-29 10:21:43 +0430

    22-loading lesson

---
+++ app/loading.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Loading(props) {
+	return (
+		<div>
+			<h1 style={{
+				width: '100vw',
+				height: '100vh',
+				display: "flex",
+				justifyContent: 'center',
+				alignItems: 'center'
+			}}>Loading...</h1>
+		</div>
+	);
+}
+
+export default Loading;
